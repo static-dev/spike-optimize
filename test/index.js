7ec09ab48826b98a0d4c0283dcb16cee67b1926e
Jeff Escalante <escalantejeff@gmail.com>
2017-07-12 15:40:44 -0400

    actual checks with test

--- test/index.js
+++ test/index.js
@@ -2,16 +2,17 @@ const optimize = require('..')
 const Spike = require('spike-core')
 const test = require('ava')
 const path = require('path')
+const fs = require('fs')
 const fixtures = path.join(__dirname, 'fixtures')
 
-test.cb('basic', (t) => {
+test.cb('full functionality', (t) => {
+  const rootPath = path.join(fixtures, 'basic')
   const project = new Spike({
-    root: path.join(fixtures, 'basic'),
+    root: rootPath,
     entry: { main: './index.js', second: './secondary.js' },
     afterSpikePlugins: [...optimize({
       scopeHoisting: true,
       aggressiveSplitting: true,
-      hashNaming: true,
       minify: true
     })]
   })
@@ -19,6 +20,12 @@ test.cb('basic', (t) => {
   project.on('error', t.end)
   project.on('warning', t.end)
   project.on('compile', (res) => {
+    const pub = path.join(rootPath, 'public')
+    const outputFiles = fs.readdirSync(pub).filter((n) => n !== 'index.html')
+    const html = fs.readFileSync(path.join(pub, 'index.html'), 'utf8')
+    outputFiles.map((f) => {
+      t.regex(html, new RegExp(f))
+    })
     t.end()
   })
 
